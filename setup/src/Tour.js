4153641e890b5c524cf1a9ad66e373c11f9cc22d
mloweedgar <mloweedgar0@gmail.com>
2022-04-04 14:05:25 +0300

    i succed to display name and price

--- setup/src/Tour.js
+++ setup/src/Tour.js
@@ -2,8 +2,18 @@ import React, { useState } from 'react';
 import Tours from './Tours';
 
 
-const Tour = ()=> {
-  return <h2>single tour component</h2>
+const Tour = ({id,image,info,price,name})=> {
+  return (
+  <article className='single-tour'>
+<img src={image} alt={name} />
+<footer>
+  <div className='tour-info'>
+    <h4>{name}</h4>
+    <h4 className='tour-price'>${price}</h4>
+  </div>
+</footer>
+  </article>
+  );
 };
 
 export default Tour;
