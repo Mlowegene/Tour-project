309d464a3ea55082ef97708b79a6f1d0bfad6b81
mloweedgar <mloweedgar0@gmail.com>
2022-04-04 14:55:29 +0300

    Not interested button work well

--- setup/src/Tour.js
+++ setup/src/Tour.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Tours from './Tours';
 
 
-const Tour = ({id,image,info,price,name})=> {
+const Tour = ({id,image,info,price,name, removeTour})=> {
   const [readMore, setReadMore] = useState(false)
   return (
   <article className='single-tour'>
@@ -19,7 +19,7 @@ const Tour = ({id,image,info,price,name})=> {
       {readMore ? 'show less' : 'read more'}
     </button>
     </p>
-  <button className='delete-btn'>not interested</button>
+  <button className='delete-btn'onClick={()=> removeTour(id)}>not interested</button>
 </footer>
   </article>
   );
